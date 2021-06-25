afc9b74f9721fc50f2177856a47ca368358c7e47
duta3135 <dutasetyawan1012@gmail.com>
2021-06-25 09:53:33 +0700

    commit

--- pages/members/index.js
+++ pages/members/index.js
@@ -1,34 +1,22 @@
-import {useState, useEffect} from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import styles from '../../styles/Members.module.css'
 import Pfp from '../../components/pfp';
 import { NameContext } from '../../NameContext';
-function Users() {
-    const [users, setUsers] = useState([]);
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(json => setUsers(json));
-    }, [])
-    
-    
+function Users({users}) {
     return (
-        
         <div className={styles.container}>
             <Head>
-            <title>members</title>
-        </Head>
+                <title>members</title>
+            </Head>
             <Link href="/" passHref><button className={styles.backBtn}>Go Back</button></Link>
             <h1 className={styles.h1}>Members</h1>
-            
             <div className={styles.list}>
                 {users.map(member=>
                     {return <div key={member.id} className={styles.memName}>
                                 <NameContext.Provider value={member.name}>
                                     <Pfp/>
                                 </NameContext.Provider>
-                                
                                 <Link href={`/members/${member.id}`}>{JSON.stringify(member.name)}</Link>
                             </div>}
                     )}
@@ -36,5 +24,12 @@ function Users() {
         </div >
     )
 }
-
+export async function getStaticProps() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await res.json()
+    return {
+      props: {
+        users,
+      },
+    }}
 export default Users
